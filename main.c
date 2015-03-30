6d9ff9424acf2bc0c8a2ee51babbb7bdf07a509d
Shane Harding <sharding@tcd.ie>
2015-03-30 09:59:09 +0100

    lksajf

--- main.c
+++ main.c
@@ -9,16 +9,39 @@ void cd(char *new_dir);
 
 int main(){
 
-  
+  pid_t pid;
   char *line;
+  int exit_flag;
 
+  pid=getpid();
+  exit_flag=0;
+  printf("%d\n", pid);
 
-  pwd();
-  line = readline("enter a command: ");
+  printf("To exit my snail shell type exit\n");
+  while(exit_flag==0){
+    pwd();
+    line = readline("snail: ");
   
- 
-  
-  pwd();
+    if(strstr(line, "ls")!=NULL){
+      pid_t child_pid = fork();
+      pid_t child_ppid=getppid();
+      printf("%d\n", child_pid);
+      if(child_pid==0){
+	execl("/bin/ls", "ls", (char *)0);
+	exit(1);
+      }
+    }
+
+    if(strstr(line, "cd")!=NULL){
+      cd((line+3));
+      //pwd();
+      printf("%s\n", (line+3));
+    }
+
+    //to exit the snail shell type exit
+    if(strcmp(line, "exit")==0)
+      exit_flag=1;
+  }
  
  
   
