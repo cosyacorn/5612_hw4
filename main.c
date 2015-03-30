028c5758c05df01a03b2ffd639ad9e6451c5d42e
Shane Harding <sharding@tcd.ie>
2015-03-30 20:26:04 +0100

    yay

--- main.c
+++ main.c
@@ -3,43 +3,72 @@
 #include<unistd.h>
 #include<readline/readline.h>
 #include<readline/history.h>
+#include<string.h>
+#include<sys/wait.h>
 
-void pwd();
-void cd(char *new_dir);
+void cd(char *cwd);
+void parse(char *line);
+void parse(char *line);
 
-int main(){
+int num_args;
+char *my_args[16];
+
+
+
+int main(int argc, char **argv, char **envp){
 
   pid_t pid;
-  char *line;
+  char *line, cwd[1024];
   int exit_flag;
 
-  pid=getpid();
+  pid=getpid();//parent id
   exit_flag=0;
   printf("%d\n", pid);
 
-  printf("To exit my snail shell type exit\n");
-  while(exit_flag==0){
-    pwd();
-    line = readline("snail: ");
-  
-    if(strstr(line, "ls")!=NULL){
-      pid_t child_pid = fork();
-      pid_t child_ppid=getppid();
-      printf("%d\n", child_pid);
-      if(child_pid==0){
-	execl("/bin/ls", "ls", (char *)0);
-	exit(1);
-      }
-    }
+  printf("\n\n\tWelcome to my snail shell!!\n\n");
+  printf("\t@             _________\n");
+  printf("\t \\____       /         \\ \n");
+  printf("\t /    \\     /   ____    \\ \n");  
+  printf("\t \\_    \\   /   /    \\    \\ \n");
+  printf("\t   \\    \\ (    \\__/  )    ) \n");
+  printf("\t    \\    \\_\\ \\______/    / \n");
+  printf("\t     \\      \\           /__ \n");
+  printf("\t      \\______\\_________/____'-_ \n");
+  printf("\n");
 
-    if(strstr(line, "cd")!=NULL){
-      cd((line+3));
-      //pwd();
-      printf("%s\n", (line+3));
+
+  printf("To exit my snail shell type exit\n\n");
+
+  while(exit_flag==0){
+    if(getcwd(cwd, sizeof(cwd))!=NULL)
+      printf("current working directory: %s\n", cwd);
+    else
+      printf("ah shit\n");
+    num_args=0;
+    line = readline("snail: ");
+    
+    parse(line);
+   
+
+    if(my_args[0]!=NULL && strcmp(my_args[0],"cd")==0){
+      cd(cwd);
+      }
+    else if(num_args>0){
+      pid_t child_pid=fork();
+      //pid_t child_ppid=getppid();//child's parent id
+      if(child_pid==0){
+	if(execvp(my_args[0], my_args)<0 && strcmp(my_args[0], "exit")!=0){
+	  printf("%s: not found\n", my_args[0]);
+	  exit(1);
+	}
+      }
+      wait(NULL);
     }
+    
+
 
     //to exit the snail shell type exit
-    if(strcmp(line, "exit")==0)
+    if(my_args[0]!=NULL && strcmp(my_args[0], "exit")==0)
       exit_flag=1;
   }
  
@@ -52,19 +81,51 @@ int main(){
 }
 
 
-void pwd(){
- 
-  char cwd[1024];
+void parse(char *line){
 
-  if(getcwd(cwd, sizeof(cwd))!=NULL)
-    printf("current working directory: %s\n", cwd);
-  else
-    printf("ah shit\n");
+  int i;
+  char *temp;
 
+  num_args=0;
+  temp=NULL;
+
+  //if(line==NULL) return;
+
+  for(i=0;i<16;i++)
+    my_args[i]=NULL;
+
+  temp=strtok(line, " ");
+
+  i=0;
+  while(temp!=NULL){
+    my_args[i]=temp;
+    num_args++;
+    i++;
+    temp=strtok(NULL, " ");
+  }
+  //printf("%s\n", my_args[0]);
 }
 
 
-void cd(char *new_dir){
-  chdir(new_dir);
 
+void cd(char cwd[1024]){
+  
+  static char prev_dir[1024];
+
+  
+
+  //procedure if '-' is give with cd
+  if(my_args[1]!=NULL && strcmp("-", my_args[1]) == 0){
+    chdir(prev_dir);
+  }
+  //if no arguments supplied
+  else if(my_args[1]==NULL){
+    chdir(getenv("HOME"));
+    printf("%s\n", getenv("HOME"));
+  }
+  else chdir(my_args[1]);
+
+  strncpy(prev_dir,cwd,1024);//store the previous directory
 }
+
+
